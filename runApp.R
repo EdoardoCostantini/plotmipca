05d2656519a5ad1291364f55ed223274e2ec02a8
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-09-12 11:43:10 +0200

    fix: adapt naming of methods to HD results file

--- runApp.R
+++ runApp.R
@@ -15,14 +15,29 @@
 # Preapre data -----------------------------------------------------------------
 
   # Data to plot
-  gg_shape <- readRDS("9950505_main_gg_shape.rds")
+  gg_shape <- readRDS("9950505_main_gg_shape.rds") # 1st BRM submission w/ correct MI-OP
+  gg_shape <- readRDS("9985893_main_gg_shape.rds") # HD version with full pj
+
+  # Map names of methods
+  methods_names <- data.frame(
+    original = c(
+      "all", "all_oracle", "aux", "vbv",
+      "MIOP", "MIOR", "MIMI", "CC", "OG"
+    ),
+    plot = c(
+      "MI-PCR-ALL", "MI-PCR-ALL (oracle)", "MI-PCR-AUX", "MI-PCR-VBV",
+      "MI-QP", "MI-OR", "MI-MI", "CC", "OG"
+    )
+  )
 
   # Change names of factors for plot
-  levels(gg_shape$method) <- c("MI-PCR-ALL", "MI-PCR-ALL (oracle)",
-                               "MI-PCR-AUX", "MI-PCR-VBV",
-                               "MI-QP", "MI-OR", "MI-MI",
-                               "CC", "OG")
+  methods_names <- methods_names[methods_names$original %in% levels(gg_shape$method), ]
+  levels(gg_shape$method) <- methods_names$plot
 
+  # Round pj
+  gg_shape$pj <- round(gg_shape$pj, 2)
+
+  # Make npc names better
   gg_shape$npc <- as.numeric(as.character(gg_shape$npc))
 
   # Graph structure
