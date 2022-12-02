7b67bb9e48d48b80a2a33e764cb71e17103309e3
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2022-12-02 18:07:52 +0100

    fix: correct deployment strategy usethis import

---
+++ app.R
@@ -0,0 +1,9 @@
+# Project:   plotmipca
+# Objective: Instruction for deployment
+# Author:    Edoardo Costantini
+# Created:   2022-12-02
+# Modified:  2022-12-02
+# Notes: 
+
+pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+plotmipca::plotmipca()
