2cffd850038e2e3fab802952d338f1286e974c09
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-04-26 16:09:13 +0200

    feat: create tabs in UI

--- R/app.R
+++ R/app.R
@@ -60,77 +60,142 @@ plotResults <- function() {
     
     # UI -----------------------------------------------------------------------
 
-    ui <- fluidPage(
-        fluidRow(
-            column(
-                4,
-                hr(),
-                h4("Data generation"),
-                radioButtons("j",
-                    "Number of observed items",
-                    choices = unique(dataResults$j),
-                    selected = unique(dataResults$j)[1],
-                    inline = TRUE
+    ui <- shiny::fluidPage(
+
+        # App title
+        shiny::titlePanel(
+            shiny::h1("High-dimensional imputation for the social sciences", align = "center")
+        ),
+        shiny::column(
+            width = 10,
+            offset = 1,
+            # Create tabs for different plotting aspects
+            shiny::tabsetPanel(
+                type = "tabs",
+                selected = "Simulation study",
+                shiny::tabPanel(
+                    title = "Simulation study",
+                    shiny::fluidRow(
+                        shiny::column(
+                            width = 3,
+                            shiny::titlePanel(
+                                shiny::h3("Simulation study", align = "center")
+                            ),
+                            shiny::tabsetPanel(
+                                type = "tabs",
+                                shiny::tabPanel(
+                                    title = "Introduction",
+                                    "Coming Soon"
+                                ),
+                                shiny::tabPanel(
+                                    title = "1. Setup",
+                                    "Coming Soon"
+                                ),
+                                shiny::tabPanel(
+                                    title = "2. Correlation matrix",
+                                    "Coming Soon"
+                                ),
+                                shiny::tabPanel(
+                                    title = "3. PC Loadings",
+                                    "Coming Soon"
+                                ),
+                                shiny::tabPanel(
+                                    title = "4. Non-graphical decision rules",
+                                    "Coming Soon"
+                                ),
+                                shiny::tabPanel(
+                                    title = "5. CPVE",
+                                    "Coming Soon"
+                                ),
+                                shiny::tabPanel(
+                                    title = "6. Conclusions",
+                                    "Coming Soon"
+                                )
+                            )
+                        ),
+                        shiny::column(
+                            width = 7,
+                            shiny::fluidRow(
+                                shiny::titlePanel(
+                                    shiny::h3("Inputs", align = "center")
+                                ),
+                                column(
+                                    4,
+                                    h4("Data generation"),
+                                    radioButtons("j",
+                                        "Number of observed items",
+                                        choices = unique(dataResults$j),
+                                        selected = unique(dataResults$j)[1],
+                                        inline = TRUE
+                                    ),
+                                    radioButtons("lv",
+                                        "Latent structure",
+                                        choices = rev(unique(dataResults$lv)),
+                                        selected = TRUE,
+                                        inline = TRUE
+                                    ),
+                                    checkboxGroupInput("K",
+                                        "Discrete levels",
+                                        inline = TRUE,
+                                        choices = levels(dataResults$K),
+                                        selected = levels(dataResults$K)[c(1, 3, 5)]
+                                    ),
+                                    checkboxGroupInput("pj",
+                                        "Proportion of noise variables",
+                                        inline = TRUE,
+                                        choices = unique(dataResults$pj),
+                                        selected = unique(dataResults$pj)[c(1, 4)]
+                                    ),
+                                ),
+                                column(
+                                    4,
+                                    h4("Outcome measures"),
+                                    selectInput("par",
+                                        "Parameter",
+                                        choices = levels(dataResults$par),
+                                        selected = "z1 correlation z2"
+                                    ),
+                                    radioButtons("plot_y_axis",
+                                        "Outcome measure",
+                                        choices = c("bias", "CIC", "CIW", "mcsd"),
+                                        inline = TRUE
+                                    ),
+                                ),
+                                column(
+                                    4,
+                                    h4("Missing data treatments"),
+                                    checkboxGroupInput("method",
+                                        "Methods",
+                                        choices = levels(dataResults$method),
+                                        selected = levels(dataResults$method)[c(1, 3:5)],
+                                        inline = TRUE
+                                    ),
+                                    shinyWidgets::sliderTextInput(
+                                        inputId = "npc",
+                                        label = "Number of principal components (NPC)",
+                                        hide_min_max = TRUE,
+                                        choices = sort(unique(dataResults$npc)),
+                                        selected = c(0, 10),
+                                        grid = TRUE
+                                    ),
+                                )
+                            ),
+                            shiny::fluidRow(
+                                shiny::titlePanel(
+                                    shiny::h3("Plot", align = "center")
+                                ),
+                                shiny::plotOutput("plot")
+                            ),
+                            style = "border-left: 1px solid; border-left-color: #DDDDDD"
+                        )
+                    )
                 ),
-                radioButtons("lv",
-                    "Latent structure",
-                    choices = rev(unique(dataResults$lv)),
-                    selected = TRUE,
-                    inline = TRUE
-                ),
-                checkboxGroupInput("K",
-                    "Discrete levels",
-                    inline = TRUE,
-                    choices = levels(dataResults$K),
-                    selected = levels(dataResults$K)[c(1, 3, 5)]
-                ),
-                checkboxGroupInput("pj",
-                    "Proportion of noise variables",
-                    inline = TRUE,
-                    choices = unique(dataResults$pj),
-                    selected = unique(dataResults$pj)[c(1, 4)]
-                ),
-            ),
-            column(
-                4,
-                hr(),
-                h4("Outcome measures"),
-                selectInput("par",
-                    "Parameter",
-                    choices = levels(dataResults$par),
-                    selected = "z1 correlation z2"
-                ),
-                radioButtons("plot_y_axis",
-                    "Outcome measure",
-                    choices = c("bias", "CIC", "CIW", "mcsd"),
-                    inline = TRUE
-                ),
-            ),
-            column(
-                4,
-                hr(),
-                h4("Missing data treatments"),
-                checkboxGroupInput("method",
-                    "Methods",
-                    choices = levels(dataResults$method),
-                    selected = levels(dataResults$method)[c(1, 3:5)],
-                    inline = TRUE
-                ),
-                shinyWidgets::sliderTextInput(
-                    inputId = "npc",
-                    label = "Number of principal components (NPC)",
-                    hide_min_max = TRUE,
-                    choices = sort(unique(dataResults$npc)),
-                    selected = c(0, 10),
-                    grid = TRUE
+                shiny::tabPanel(
+                    title = "Case study",
+                    "Coming soon"
                 ),
             )
-        ),
-
-        # Silent extraction of size
-        shinybrowser::detect(),
-        hr(),
-        plotOutput("plot"),
+        )
     )
 
     # Server -------------------------------------------------------------------
