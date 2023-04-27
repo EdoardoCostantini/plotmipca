c1a90c5339428a149008c8531adea5c47fe0c345
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-04-27 10:21:45 +0200

    feat: modularise main simulation plot

--- R/server.R
+++ R/server.R
@@ -21,53 +21,23 @@ server <- function(input, output, session) {
 
     # Simulation study plot ------------------------------------------------
 
-    output$plot <- renderPlot(res = 96, height = 500, {
-        dataResults %>%
-            filter(
-                j == input$j,
-                par == input$par,
-                lv == input$lv,
-                method %in% input$method,
-                npc <= input$npc[2],
-                npc >= input$npc[1],
-                K %in% input$K,
-                pj %in% input$pj
-            ) %>%
-            mutate(npc = factor(npc)) %>%
-            ggplot(aes_string(
-                x = "K",
-                y = input$plot_y_axis,
-                group = "npc",
-                fill = "NPC"
-            )) +
-            geom_bar(
-                stat = "identity",
-                position = "dodge",
-                colour = "black",
-                size = .25
-            ) +
-            scale_fill_manual(
-                values = c(gray.colors(2, start = 0.5, end = 0.8), "white")
-            ) +
-            facet_grid(reformulate("method", "pj"),
-                labeller = labeller(.rows = label_both, .cols = label_value),
-                scales = "free",
-                switch = "y"
-            ) +
-            theme(
-                # Text
-                text = element_text(size = 12),
-                strip.text.y.right = element_text(angle = 0),
-                # Legend
-                legend.position = "right",
-                # Backgorund
-                panel.background = element_rect(fill = NA, color = "gray")
-            ) +
-            labs(
-                x = "K",
-                y = input$plot_y_axis
+    output$plot <- renderPlot(
+        res = 96,
+        height = 500,
+        {
+            plot_simulation(
+                results = dataResults,
+                n_items = input$j,
+                parameter = input$par,
+                latent_structure = input$lv,
+                method_vector = input$method,
+                npc_range = input$npc,
+                categories = input$K,
+                prop_noise = input$pj,
+                outcome = input$plot_y_axis
             )
-    })
+        }
+    )
 
     # Simulation study traceplots ------------------------------------------
 
